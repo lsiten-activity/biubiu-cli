729215dd68bf68ccd40cafa199b4257a5348aeac
雷诗城 <leishicheng@MacBook-Pro-3.local>
2021-04-09 16:30:09 +0800

    cli

---
+++ tpl/biubiu.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'biubiu-template',
+  templateName: '营销活动',
+  author: 'lsiten',
+  snapshot: 'https://cdn.img.snapshot.png',
+  gitUrl: 'git@github.com:lsiten-activity/biubiu-template.git',
+  projectType: '0'
+};
